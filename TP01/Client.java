5fa46cef288c77aa07cdee2cc0f5ed6deae0af7b
Clement Decoopman <decoopmc@ens-epua.univ-savoie.fr>
2019-09-20 15:42:29 +0200

    Establishing client and single socket server

---
+++ TP01/Client.java
@@ -0,0 +1,81 @@
+import java.net.*;
+import java.io.*;
+
+
+
+/**
+ * Client socket class
+
+ * @author decoopmc
+ * @version 1.0
+ * @see java.net
+ */
+public class Client {
+
+	/**
+	 * Connection host
+	 */
+	private static String host = "localhost";
+
+	/**
+	 * Connection port
+	 */
+	private static int port = 1600;
+
+	/**
+	 * Simple TimeStamp to track dates from messages
+	 */
+	private static String timeStamp = "";
+
+
+	/**
+	 * MAIN
+	 */
+	public static void main(String[] args) 
+	{
+		/* INITIALIZING CLIENT */
+		StringBuffer instr = new StringBuffer();
+		System.out.println("Client initialized");
+
+		/* ESTABLISHING CONNECTION */
+		try {
+			InetAddress address     = InetAddress.getByName(host);		// address object containing the couple (name,host)
+			Socket connection       = new Socket(address, port); 		// instanciate a socket connection
+
+			//Writing message
+			timeStamp      = new java.util.Date().toString();
+	    	String message = "Calling the Socket Server on "+ host + " port " + port + " at " + timeStamp +  (char) 13;
+			write("", connection);
+		}
+		catch(IOException e) {}
+	}
+
+
+	/**
+	 * Write in socket
+	 */
+	public static void write(String message, Socket connection)
+	{
+		try {
+			BufferedOutputStream os = new BufferedOutputStream(connection.getOutputStream()); 	// container for writing to the socket
+			OutputStreamWriter osw  = new OutputStreamWriter(os, "US-ASCII");					// instanciate a writer
+		    
+		    // Write across the socket connection and flush the buffer
+		    osw.write(message);
+		    osw.flush();
+		}
+		catch(IOException e) {}
+		
+	}
+
+
+	/**
+	 * Read from socket
+	 */
+	/*public void read()
+	{
+
+	}*/
+
+
+}
---
+++ TP01/Server.java
@@ -0,0 +1,108 @@
+import java.net.*;
+import java.io.*;
+import java.util.*;
+
+
+
+/**
+ * Single server socket class
+
+ * @author decoopmc
+ * @version 1.0
+ * @see java.net
+ */
+public class SingleServer {
+
+	/**
+	 *
+	 */
+	static ServerSocket socket1;
+
+	/**
+	 *
+	 */
+	static Socket connection;
+
+	/**
+	 *
+	 */
+	protected final static int port = 1600;
+
+	/**
+	 *
+	 */
+	static boolean first;
+
+	/**
+	 *
+	 */
+	static StringBuffer message;
+
+	/**
+	 *
+	 */
+	static String timeStamp;
+
+
+	/**
+	 * MAIN
+	 */
+	public static void main(String[] args) 
+	{
+		try {
+			//INITIALIZING SERVER
+			socket1 = new ServerSocket(port);
+			System.out.println("SingleSocketServer initialized");
+			int character;
+
+			//SERVER LOOP
+			while(true) {
+				connection = socket1.accept();
+				BufferedInputStream is = new BufferedInputStream(connection.getInputStream());
+	        	InputStreamReader isr  = new InputStreamReader(is);
+	        	message = new StringBuffer();
+
+	        	while((character = isr.read()) != 13) {
+	        		message.append((char)character);
+	        	}
+
+	        	//Printing received message
+	        	System.out.println(message);
+
+	        	//waiting 10 sec for the app to update database
+	        	try {
+	        		Thread.sleep(10000);
+	        	}
+	        	catch(Exception e) {}
+
+	        	//Sending response
+	        	timeStamp    = new java.util.Date().toString();
+	        	String returnCode = "SingleSocketServer responded at " + timeStamp + (char) 13;
+		        write(returnCode);
+			}
+		}
+		catch(IOException e) {}
+
+		//CLOSING CONNECTION
+		try {
+			connection.close();
+		}
+		catch(IOException e) {}
+	}
+
+
+	/**
+	 * Writing message for clients
+	 */
+	public static void write(String message)
+	{
+		try {
+			BufferedOutputStream os = new BufferedOutputStream(connection.getOutputStream());
+	        OutputStreamWriter osw  = new OutputStreamWriter(os, "US-ASCII");
+	        osw.write(message);
+	        osw.flush();
+		}
+		catch(IOException e) {}
+	}
+
+}
